1d6940feb6dcc7f85a0e52a62a93a844dd2facfc
leo <junkainiu@gmail.com>
2015-12-29 19:24:06 +0800

    a good fib algorithom

--- python/lv3/fib.py
+++ python/lv3/fib.py
@@ -6,13 +6,18 @@ def mul(a, b):
   r[1][1] = a[1][0] * b[0][1] + a[1][1] * b[1][1];
   return r;
 
-# 递归加速计算斐波那契数列 O(n^2) -> O(logn)
-def fib(n):
+def _fib(n):
   if n == 0:
     return [[1, 0], [0, 1]]
   if n == 1:
     return [[1, 1], [1, 0]]
   if n % 2 == 0 :
-    return mul(fib(int(n / 2)), fib(int(n / 2)))
+    return mul(_fib((n / 2)), _fib((n / 2)))
   else:
-    return mul(mul(fib(int(n / 2)), fib(int(n / 2))), [[1, 1], [1, 0]])
+    return mul(mul(_fib((n-1) / 2), _fib((n-1) / 2)), [[1, 1], [1, 0]])
+
+def fib(n):
+  if n<0:
+    return _fib(-n)[1][0] * (1 if n%2 == 1 else -1)
+  else:
+    return _fib(n)[1][0]
